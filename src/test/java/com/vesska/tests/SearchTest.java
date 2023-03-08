f3e913cab2653d19477dec763f1cb87967e055d5
Sergei Savich <s.savich@ipoteka.center>
2023-03-08 17:31:23 +0300

    add search test and telegram not

--- src/test/java/com/vesska/tests/SearchTest.java
+++ src/test/java/com/vesska/tests/SearchTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.accessibilityId;
@@ -16,7 +17,20 @@ public class SearchTest extends TestBase {
 
     @Test
     @Tag("android")
-    void searchForGermanyPageTest() {
+    void searchTest() {
+        back();
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+
+    }
+    @Test
+    @Tag("android")
+    void openGermanyPageTest() {
         back();
         step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
